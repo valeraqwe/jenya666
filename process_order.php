0ae6a6c900d7f088bb0b415e067bc05528d82fd6
Valeriy <valera.komar.1999@gmail.com>
2024-06-16 16:15:57 +0300

    1

--- process_order.php
+++ process_order.php
@@ -1,6 +1,7 @@
 <?php
-$token = "YOUR_BOT_TOKEN";
-$chat_id = "YOUR_CHAT_ID";
+$token = "7321335943:AAFH2vzjL6_sAfofa-p7-G5Frduhs3jBCfU";
+$chat_id = "-4238094603";
+
 
 $customer_name = $_POST['customer_name'];
 $customer_phone = $_POST['customer_phone'];
